da9ee616a2a2908497acb50a87db09f5e82471e7
Muhammad Rabi <vonrabi07@yahoo.com>
2023-04-27 00:52:11 +0200

    Add register page

--- src/components/LoginCover.jsx
+++ src/components/LoginCover.jsx
@@ -2,11 +2,7 @@ import coverImage from "../assets/images/logincover.jpg";
 
 const LoginCover = () => {
   return (
-    <img
-      src={coverImage}
-      className="flex-none object-cover basis-7/12"
-      alt="cover-img"
-    />
+    <img src={coverImage} className="object-cover basis-7/12" alt="cover-img" />
   );
 };
 export default LoginCover;
--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -9,11 +9,11 @@ const Cart = () => {
     <>
       <Link to="cart">
         <div className="hidden px-4 py-1 rounded-full cursor-pointer lg:block md:px-8">
-          <div className="relative flex space-x-4 text-4xl duration-75">
+          <div className="relative flex space-x-4 text-3xl duration-75">
             <AiOutlineHeart />
             <AiOutlineShoppingCart />
           </div>
-          <p className="absolute w-6 h-6 flex justify-center items-center rounded-full text-[18px] font-bold p-4 top-0 right-32 bg-red-700 z-10 text-white">
+          <p className="absolute w-4 h-4 flex justify-center items-center rounded-full text-xs p-3 top-2 right-32 bg-red-700 z-10 text-white">
             {itemsInCart.length}
           </p>
         </div>
