14d5c8116eb6d87347e79507c6636ce80120fc78
Muhammad Rabi <vonrabi07@yahoo.com>
2023-07-15 21:01:45 +0300

    Fix overlay onclick

--- src/components/UI/Overlay.jsx
+++ src/components/UI/Overlay.jsx
@@ -1,8 +1,22 @@
+import { useCartStore } from "../../Features/cartStore"
+import { useModalStore } from "../../Features/modalStore"
+
 const Overlay = ({ children }) => {
+  const closeModal = useModalStore((state) => state.closeModal)
+  const closeCart = useCartStore((state) => state.closeCart)
+  const closeConfirmation = useModalStore((state) => state.closeConfirmation)
+
   return (
-    <div className="absolute top-0 left-0 z-40 flex flex-col items-center justify-start w-full h-full bg-black/70">
+    <div
+      className="absolute top-0 left-0 z-40 flex flex-col items-center justify-start w-full h-full bg-black/70"
+      onClick={() => {
+        closeModal()
+        closeCart()
+        closeConfirmation()
+      }}
+    >
       {children}
     </div>
-  );
-};
-export default Overlay;
+  )
+}
+export default Overlay
