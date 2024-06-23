79d94aebcc9279bb0c263bafc0bb1cd3431d1509
Muhammad Rabi <vonrabi07@yahoo.com>
2024-06-23 20:16:19 +0300

    update api url

--- src/Features/productStore.jsx
+++ src/Features/productStore.jsx
@@ -1,5 +1,5 @@
-import { create } from "zustand";
-import { devtools } from "zustand/middleware";
+import { create } from "zustand"
+import { devtools } from "zustand/middleware"
 // import { appData } from "../utils/data";
 
 const products = (set) => ({
@@ -10,19 +10,20 @@ const products = (set) => ({
 
   //fetching data
   getProducts: async () => {
-    const url = "https://course-api.com/react-store-products";
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      set({ productsList: data });
+    const url = "https://www.course-api.com/react-store-products"
+    //www.course-api.com/react-store-products
+    https: try {
+      const response = await fetch(url)
+      const data = await response.json()
+      set({ productsList: data })
       set({
         featuredProducts: data.filter((item) => item.featured === true),
         isLoading: false,
-      });
+      })
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   },
-});
+})
 
-export const useProductStore = create(devtools(products));
+export const useProductStore = create(devtools(products))
--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,30 +1,30 @@
-import { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
-import ProductDetails from "../components/product/ProductDetails";
-import Layout from "../components/UI/Layout";
-import ProductDetailsSkeleton from "../components/product/ProductDetailsSkeleton";
+import { useEffect, useState } from "react"
+import { useParams, Link } from "react-router-dom"
+import ProductDetails from "../components/product/ProductDetails"
+import Layout from "../components/UI/Layout"
+import ProductDetailsSkeleton from "../components/product/ProductDetailsSkeleton"
 
 const ProductDetailPage = () => {
   const single_product_url =
-    "https://course-api.com/react-store-single-product?id=";
-  const { id } = useParams();
-  const [singleProduct, setSingleProduct] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+    "https://www.course-api.com/react-store-single-product?id="
+  const { id } = useParams()
+  const [singleProduct, setSingleProduct] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
   const fetchSingleProduct = async (id) => {
     try {
-      setIsLoading(true);
-      const res = await fetch(`${single_product_url}${id}`);
-      const data = await res.json();
-      setSingleProduct(data);
-      setIsLoading(false);
+      setIsLoading(true)
+      const res = await fetch(`${single_product_url}${id}`)
+      const data = await res.json()
+      setSingleProduct(data)
+      setIsLoading(false)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   useEffect(() => {
-    fetchSingleProduct(id);
-  }, [setSingleProduct, id]);
+    fetchSingleProduct(id)
+  }, [setSingleProduct, id])
 
   return (
     <Layout>
@@ -34,6 +34,6 @@ const ProductDetailPage = () => {
       <ProductDetails {...singleProduct} />
       {isLoading && <ProductDetailsSkeleton />}
     </Layout>
-  );
-};
-export default ProductDetailPage;
+  )
+}
+export default ProductDetailPage
